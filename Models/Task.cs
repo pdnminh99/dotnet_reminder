1b653d9e3ef231ec6940b611307012a1220ed9e5
pdnminh99 <pdn.minh1999@gmail.com>
2020-10-29 10:55:35 +0700

    Add generated fields for Task model

--- Models/Task.cs
+++ Models/Task.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace Reminder.Models
 {
     public class Task
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid TaskId { get; set; }
 
         public string Content { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        [DefaultValue("")] public string Note { get; set; }
 
         public DateTime CreationDate { get; set; }
 
