50021c4ffb6afd4a03d0db215ad66bb08cf83d44
pdnminh99 <pdn.minh1999@gmail.com>
2020-10-30 22:30:56 +0700

    update README

--- Models/Collection.cs
+++ Models/Collection.cs
@@ -8,19 +8,19 @@ namespace Reminder.Models
 {
     public class Collection
     {
+#nullable enable
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int? CollectionId { get; set; }
-
-        [Required(AllowEmptyStrings = false, ErrorMessage = "`Name` field must not empty or null.")]
-        public string Name { get; set; }
 
         public DateTime? CreationDate { get; set; }
 
         public DateTime? LastEdited { get; set; }
 
-#nullable enable
         [JsonIgnore] public virtual User? Owner { get; set; }
 #nullable disable
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "`Name` field must not empty or null.")]
+        public string Name { get; set; }
 
         [JsonIgnore] public virtual List<Task> Tasks { get; set; }
     }
--- README.md
+++ README.md
@@ -8,14 +8,20 @@ Subject: .NET 20.1A
 
 Run the following commands in exact order: 
 
-- This will install code generation tool
+ - This will install code generation tool
 
 `dotnet tool install -g dotnet-aspnet-codegenerator`
+ 
+ - This will init migrations
+ 
+ `dotnet ef migrations add [name]`
  
  - This will apply migration configs (which located in the `Migrations` folder):
  
 `dotnet ef database update`
 
-## Drop database
+## Drop database & Remove migrations
 
 `dotnet ef database drop`
+
+`dotnet ef migrations remove`
